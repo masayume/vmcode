1f96bc6765bdf33ddc9c82db23538c660675ee7d
masayume <masayume.blog@gmail.com>
2014-08-05 16:52:14 +0200

    revised 4th version and added simple 3-3-4 verse cyberpoetry generator

---
+++ grm/poetry.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo "\n --------- "
+polygen inspire4.grm
+
+echo ""
+for i in `seq 3`; do polygen inspire4.grm; done
+
+echo ""
+for i in `seq 3`; do polygen inspire4.grm; done
+
+echo ""
+for i in `seq 4`; do polygen inspire4.grm; done
+
+echo "\n --------- "
+
