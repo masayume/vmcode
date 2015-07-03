ea79632f58fd8cde8dcaf9f4877e390e490a03b5
masayume <masayume.blog@gmail.com>
2015-07-03 17:11:45 +0200

    rory: layout upgrade

--- rory.php
+++ rory.php
@@ -1,8 +1,9 @@
 <?php
 
-$path = "/rory-story/";
-$dice = array(); // story-d1-1.jpg
-$packs = array("story");
+$path = "/VM/rory/story/";		// directory immagini
+$dice = array(); // story-d1-1.jpg	// array del dado globale
+$rdie = array();
+$packs = array("story");		
 
 $extr	= 3;
 $size	= "250px";
@@ -11,28 +12,52 @@ if ($_GET['dice']) {
 	$extr = $_GET['dice'];
 }
 
+// DICE DEFINITION LOOP
+
 foreach ($packs as $pack) {
 	for ($i=1; $i<=9; $i++) { // dices
 		for ($j=1; $j<=6; $j++) { //faces
 			$die = $pack . "-d" . $i . "-" . $j . ".jpg";
 			array_push($dice, $die);
+			array_push($rdie[$i], $die);
 		}
 	}
 }
-// print "<pre>"; print_r($dice);
+
+// SHUFFLE
 
 shuffle($dice);
 
+// DIE PREPARATION
+
 $dice4page = "";
 for ($k=0; $k<$extr; $k++) {
-	$dice4page .= "<img src=\"" . $path . array_pop($dice) . "\" style=\"width:$size\">";
+	$dice4page .= "<div class=\"_exa\"><img src=\"" . $path . array_pop($dice) . "\" style=\"width:$size\"></div>";
 }
 
-$page = <<< EOP
-	$dice4page
+// GLOBAL PAGE LAYOUT
 
+$page = <<< EOP
+<html>
+<head>	
+	<style>
+.container{ text-align:center; border:1px solid #666; }
+._img{ display:inline-block; margin:5px 20px; padding:5px; border:1px solid #CCC; }
+._text{ display:inline-block; margin:5px 20px; padding:5px; border:1px solid #CCC; top: 0px; }
+._exa{ display:inline-block; margin:5px 6px; padding:5px; border:1px solid #CCC; top: 0px; vertical-align: text-top;}
+		
+	</style>
+</head>
+<body>
+<div class="container">
+	$dice4page
+</div>
+</body>
+</html>
 EOP;
 
 print $page;
+
+
 
 exit(0);
