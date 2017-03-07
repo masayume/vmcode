c2895e5e49dd30164c28d461a5dc81281eb5522d
masayume <masayume.blog@gmail.com>
2017-03-07 15:56:26 +0100

    mojs children1 dynamic + json

--- html5/mojs/index.php
+++ html5/mojs/index.php
@@ -2,41 +2,25 @@
 <html >
 <head>
   <meta charset="UTF-8">
-  <title>Intro to Mo.js</title>
-  
-  
-  
+  <title>Mo.js app</title>
       <link rel="stylesheet" href="css/style.css">
-
-  
 </head>
-
 <body>
 
+<a href="http://mojs.io/tutorials/shape/">tut</a>
   <script src='js/mo.min.js'></script>
-
   <script type="text/javascript">
 
 <?php
 
-$js = <<<"EOJ"
-'use strict';
+global $config;
 
-var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+$configfile = file_get_contents("mojs.json");
+$config = json_decode($configfile, true);
 
-var burst = new mojs.Burst({
-  radius: { 0: 150 },
-  count: 20,
-  children: {
-    shape: 'cross',
-    stroke: 'red',
-    strokeWidth: { 6: 0 },
-    angle: { 360: 0 },
-    radius: { 50: 5 },
-    duration: 3000
-  }
-});
+$burst1 = burst();
 
+$burst2 = <<<"EOB"
 var burst2 = new mojs.Burst({
   radius: { 0: 200 },
   count: 12,
@@ -52,7 +36,9 @@ var burst2 = new mojs.Burst({
     duration: 3000
   }
 });
+EOB;
 
+$burst3 = <<<"EOB"
 var burst3 = new mojs.Burst({
   radius: { 0: 250 },
   count: 7,
@@ -66,6 +52,9 @@ var burst3 = new mojs.Burst({
   }
 });
 
+EOB;
+
+$circ1 = <<<"EOC"
 var circ_opt = {
   radius: { 0: 100 },
   fill: 'grey',
@@ -74,6 +63,12 @@ var circ_opt = {
   duration: 3500
 };
 
+var circ1 = new mojs.Shape(_extends({}, circ_opt, {
+  delay: 1000
+}));
+EOC;
+
+$circ2 = <<<"EOC"
 var circ_opt2 = {
   radius: { 0: 300 },
   fill: 'none',
@@ -82,13 +77,26 @@ var circ_opt2 = {
   duration: 3500
 };
 
-var circ1 = new mojs.Shape(_extends({}, circ_opt, {
-  delay: 1000
-}));
-
 var circ2 = new mojs.Shape(_extends({}, circ_opt2, {
   delay: 500
 }));
+EOC;
+
+
+$js = <<<"EOJ"
+'use strict';
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+$burst1
+
+$burst2
+
+$burst3
+
+$circ1
+
+$circ2
 
 var timeline = new mojs.Timeline({
   repeat: 999
@@ -96,6 +104,77 @@ var timeline = new mojs.Timeline({
 EOJ;
 
 print $js;
+
+function burst() {
+
+$radius 	= 50 * rand(1,10);
+$count 		= 2 * rand(1,10);
+
+$children 	= children();
+
+$burst1 = <<<"EOB"
+var burst = new mojs.Burst({
+  radius: { 0: $radius },
+  count: $count,
+  $children
+});
+EOB;
+
+return $burst1;
+
+}
+
+
+function children() {
+
+global $config;
+
+$radius1 	= 50 * rand(1,10);
+$radius2 	= 50 * rand(1,10);
+$duration 	= 600 * rand(1,10);
+
+$shape 		= $config['config']['children']['params']['shape'][array_rand($config['config']['children']['params']['shape'], 1)];
+
+$stroke1 	= stroke1(); 
+
+$children = <<<"EOC"
+  children: {
+    shape: '$shape',
+    $stroke1
+    strokeWidth: { 6: 0 },
+    angle: { 360: 0 },
+    radius: { $radius1: $radius2},
+    duration: $duration
+  }
+EOC;
+
+return $children;
+
+} // end function burst
+
+function stroke1() {
+//   stroke: { 'orange': 'yellow' },
+global $config;
+
+$stroke1 	= $config['config']['children']['params']['stroke1'][array_rand($config['config']['children']['params']['stroke1'], 1)];
+
+return "stroke: '$stroke1',";
+
+} // end function stroke
+
+
+function stroke2() {
+//   stroke: { 'orange': 'yellow' },
+global $config;
+
+$stroke1 	= $config['config']['children']['params']['stroke1'][array_rand($config['config']['children']['params']['stroke1'], 1)];
+$stroke2 	= $config['config']['children']['params']['stroke2'][array_rand($config['config']['children']['params']['stroke2'], 1)];
+
+return "stroke: { '$stroke1': '$stroke2' }";
+
+} // end function stroke
+
+
 
 ?>
 
