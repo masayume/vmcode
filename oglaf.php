06ab0cd5191cf1cb0fcd7c77f169794272820f2e
masayume <masayume.blog@gmail.com>
2017-06-16 16:47:48 +0200

    oglaf random image from directory: working

--- oglaf.php
+++ oglaf.php
@@ -1,12 +1,18 @@
 <?php
 
-$oglafiledir 	= '/var/www/html/inspire/@CARTOONS/Oglaf'
+$oglafiledir 	= '/var/www/html/inspire/@CARTOONS/Oglaf/';
 
 $images = glob($oglafiledir . '*.{jpg,jpeg,png,gif}', GLOB_BRACE);
 
-$randomImage = $images[array_rand($images)]; // See comments
+$randomImage 	= $images[array_rand($images)]; // See comments
 
-print $randomImage;
+$rndImgUrl 	= $randomImage;
+
+$pattern 	= "/\/var\/www\/html/";
+$replacement 	= "";
+$rndImgUrl 	=  preg_replace($pattern, $replacement, $rndImgUrl);
+
+print "<img src='$rndImgUrl'>";
 
 
 
