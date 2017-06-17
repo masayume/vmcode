4e4a92dcc9399337787da79d21957e9eb19eb48b
masayume <masayume.blog@gmail.com>
2017-06-17 02:28:32 +0200

    oglaf random image from directory: scale to cover

--- oglaf.php
+++ oglaf.php
@@ -12,7 +12,42 @@ $pattern 	= "/\/var\/www\/html/";
 $replacement 	= "";
 $rndImgUrl 	=  preg_replace($pattern, $replacement, $rndImgUrl);
 
-print "<img src='$rndImgUrl'>";
+$html = <<<EOH
+<html>
+<head>
+<style type="text/css">
 
+.cover{
+    color: rgb(255, 255, 255);
+    position: relative;
+    min-height: 700px;
+    background: url("$rndImgUrl") no-repeat scroll 0px 100% / cover transparent;
+}
 
+nav ul li {
+  list-style:none;
+  float:left;
+  margin-right:50px;
 
+}
+
+}
+</style>
+</head>
+<body>
+
+<div class="cover">
+  <div class="clearfix"></div>
+   <nav>
+    <ul>
+	<li>&nbsp;</li>
+	<li>&nbsp;</li>
+    </ul>
+  </nav>
+</div>
+
+</body>
+</html>
+EOH;
+
+print $html;
