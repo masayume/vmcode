cb8f30e38726d0d6587c43bde2a9a53405817f7d
masayume <masayume.blog@gmail.com>
2022-12-01 01:18:18 +0100

    new ai.php page

---
+++ home/ai.php
@@ -0,0 +1,297 @@
+<html>
+<head>
+<title>AI MODELS PAGE</title>
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/home.css" rel="stylesheet">
+    <meta charset="UTF-8">
+<style>
+  body { 
+/*	background-image:url('img/ub-wallp.jpg'); 
+	background-color: #0033cc;
+*/
+
+/* Permalink - use to edit and share this gradient: http://colorzilla.com/gradient-editor/#1e5799+0,2989d8+50,207cca+51,7db9e8+100;Blue+Gloss+Default */
+	background: rgb(130,87,53); /* Old browsers */
+	background: -moz-linear-gradient(-45deg, rgba(130,27,53,1) 0%, rgba(141,37,16,1) 50%, rgba(125,85,22,1) 100%); /* FF3.6-15 */
+	background: -webkit-linear-gradient(-45deg, rgba(130,27,53,1) 0%,rgb(141, 37, 16, 1) 50%,rgba(125,85,22,1) 100%); /* Chrome10-25,Safari5.1-6 */
+	background: linear-gradient(135deg, rgba(130,27,53,1) 0%,rgba(141,37,16,1) 50%,rgba(125,185,232,1) 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+	filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#1e5799', endColorstr='#7db9e8',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
+  }
+  a, a:visited {  color: #ffffff; }
+  h3, h4 {  color: #00aaff; }
+  .container{ text-align:left; border:1px solid #666; }
+  ._img{ display:inline-block; margin:5px 20px; padding:5px; border:1px solid #CCC; }
+  ._text{ display:inline-block; margin:5px 20px; padding:5px; border:1px solid #CCC; top: 0px; }
+  ._exa{ display:inline-block; margin:5px 6px; padding:5px; border:1px solid #CCC; top: 0px; vertical-align: text-top;}
+</style>
+<!-- Matomo -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="/matomo/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Matomo Code -->
+<script type="text/javascript" src="index-gifs.js"></script>
+
+</head>
+<body>
+<div class="container-fluid">
+
+  <div class="_exa">
+    <h4>AI models page</h4>
+      <ul>
+        <li> <a href="pixelart.htm" target="_blank"><b>PIXELART page</b></a> </li>
+        <li> <a href="tools.htm" target="_blank"><b>TOOLS page</b></a> </li>
+        <li> <a href="unity.htm" target="_blank"><b>UNITY page</b></a> </li>
+        <li> <a href="inspire.htm" target="_blank"><b>INSPIRE page</b></a> </li>
+        <li> <a href='/training'>training</a> <a href="https://www.masayume.it/training">(remote)</a></li>
+        <li> <a href="retro.htm" target="_blank"><b>RETRO page</b></a> </li>
+      </ul>
+
+    <h4>GALLERIES</h4>
+      <ul>
+        <li> <a href="https://twitter.com/GeneratedImage/media" target="_blank"><b>GeneratedImage</b></a> </li>
+        <li> <a href="https://www.krea.ai/" target="_blank"><b>krea.ai</b></a> </li>
+        <li> <a href="https://lexica.art/" target="_blank"><b>lexica.art</b></a> </li>
+        <li> <a href="https://libraire.ai/" target="_blank"><b>libraire.ai</b></a> </li>
+      </ul>
+
+  </div>
+
+  <div class="_exa">
+    <h4>TOOLS</h4>
+    <ul>
+      <li> <a href="https://www.futurepedia.io/"><b>futurepedia.io ★</b></a> </li>
+            <li> <a href="https://huggingface.co/spaces?sort=likes"><b>huggingface spaces ★★</b></a></li> 
+            <li> <a href="https://lexica.art/"><b>lexica.art ★★</b></a></li> 
+            <li> <a href="https://libraire.ai/"><b>libraire.ai ★★</b></a></li> 
+            <li> <a href="https://huggingface.co/hakurei/waifu-diffusion"><b>waifu diffusion ★★</b></a></li>
+            <li> <a href="https://huggingface.co/spaces/akhaliq/AnimeGANv2"><b>AnimeGANv2 ★</b></a></li>
+            <li> <a href="https://huggingface.co/spaces/stabilityai/stable-diffusion"><b>stablediffusion 🤗★</b></a>
+                 <a href="https://stablediffusionweb.com/"><b>site</b></a></li>
+            <li> <a href="https://twitter.com/hashtag/stablediffusionart"><b>#stablediffusion★</b></a></li>  
+            <li> <a href="https://twitter.com/hashtag/NovelAIDiffusion?src=hashtag_click&f=live"><b>#NovelAIDiff★</b></a>
+                 <a href="https://twitter.com/hashtag/NovelAI?src=hashtag_click&f=live"><b>#NovelAI★</b></a></li>  
+            <li><a href="https://discord.com/channels/662267976984297473/989268365870776411"><b>midjourney</b></a></a></li>
+            <li> <a href="https://weirdwonderfulai.art/"><b>Weird Wonderful AI</b></a> </li>
+            <li> <a href="https://colab.research.google.com/github/huggingface/notebooks/blob/main/diffusers/stable_diffusion.ipynb"><b>Stable Diffusion ★</b></a> </li>
+            <li> <a href="https://www.masayume.it/blog/content/disco-diffusion-google-colab"><b>Disco Diffusion ★</b></a> </li>    
+            <li><a href="https://twitter.com/search?q=discodiffusion%20min_faves%3A20&src=typed_query&f=top"><b>discodiffusion ADVQ ■★</b></a></li>
+            <li><a href="https://twitter.com/nin_artificial/media"><b>nin_artificial</b></a></li>
+            <li><a href="https://www.reddit.com/r/deepdream/"><b>Red.Deepdream</b></a></li>
+            <li><a href="https://twitter.com/GlennIsZen/media"><b>GlennIsZen</b></a>
+                <a href="https://twitter.com/nvnot_/media"><b>NVnot</b></a>
+                <a href="https://twitter.com/snecc_art/media">snecc</a></li>
+            <li><a href="https://twitter.com/ganbrood/media"><b>ganbrood</b></a>
+                <a href="https://twitter.com/Somnai_dreams/media"><b>Somnaidrms</b></a></li>
+            <li><a href="https://twitter.com/romanguillermo_/media"><b>romanguillermo</b></a></li>
+            <li><a href="https://twitter.com/EErratica/media"><b>EncyclErratica</b></a>
+                <a href="https://twitter.com/NekroXIII/media"><b>NecroXIII</b></a></li>
+            <li><a href="https://twitter.com/JvdeNft/media">JvdeNft</a>
+                <a href="https://twitter.com/inigma_a/media">AInigma</a>
+                <a href="https://twitter.com/thibaudz/media">Thibaudz</a></li>
+            <li><a href="https://twitter.com/noir_random/media">noirndom</a>
+                <a href="https://twitter.com/EmporiumLoris/media">LorisEmporium</a></li>
+            <li><a href="https://twitter.com/magnasoma/media">magnasoma</a></li>
+    </ul>
+    
+  </div>
+
+  <div class="_exa">
+
+    <h4>HUGGINGFACE</h4>
+    <ul>
+      <li> <a href="URL">SPACE #1</a> </li>
+      <li> <a href="URL">SPACE #2</a> </li>
+      <li> <a href="URL">SPACE #3</a> </li>
+      <li> <a href="URL">SPACE #4</a> </li>
+    </ul>
+
+    <h4>TITLE #6</h4>
+    <ul>
+      <li> <a href="URL">LINK #14</a> </li>
+      <li> <a href="URL">LINK #15</a> </li>
+      <li> <a href="URL">LINK #16</a> </li>
+      <li> <a href="URL">LINK #17</a> </li>
+    </ul>
+
+</div>
+
+
+  <div class="_exa">
+
+    <details>
+        <summary><strong><b>DIFFUSION MODELS ⬇️</b></strong></summary> <!-- ALSO on INSPIRE,UNITY,PIXELART,TOOLS -->
+        <p>
+          <ul>
+            <li> <a href="https://huggingface.co/spaces?sort=likes"><b>huggingface spaces ★★</b></a></li> 
+            <li> <a href="https://lexica.art/"><b>lexica.art ★★</b></a></li> 
+            <li> <a href="https://libraire.ai/"><b>libraire.ai ★★</b></a></li> 
+            <li> <a href="https://huggingface.co/hakurei/waifu-diffusion"><b>waifu diffusion ★★</b></a></li>
+            <li> <a href="https://huggingface.co/spaces/akhaliq/AnimeGANv2"><b>AnimeGANv2 ★</b></a></li>
+            <li> <a href="https://huggingface.co/spaces/stabilityai/stable-diffusion"><b>stablediffusion 🤗★</b></a>
+                 <a href="https://stablediffusionweb.com/"><b>site</b></a></li>
+            <li> <a href="https://twitter.com/hashtag/stablediffusionart"><b>#stablediffusion★</b></a></li>  
+            <li> <a href="https://twitter.com/hashtag/NovelAIDiffusion?src=hashtag_click&f=live"><b>#NovelAIDiff★</b></a>
+                 <a href="https://twitter.com/hashtag/NovelAI?src=hashtag_click&f=live"><b>#NovelAI★</b></a></li>  
+            <li><a href="https://discord.com/channels/662267976984297473/989268365870776411"><b>midjourney</b></a></a></li>
+            <li> <a href="https://weirdwonderfulai.art/"><b>Weird Wonderful AI</b></a> </li>
+            <li> <a href="https://colab.research.google.com/github/huggingface/notebooks/blob/main/diffusers/stable_diffusion.ipynb"><b>Stable Diffusion ★</b></a> </li>
+            <li> <a href="https://www.masayume.it/blog/content/disco-diffusion-google-colab"><b>Disco Diffusion ★</b></a> </li>    
+            <li><a href="https://twitter.com/search?q=discodiffusion%20min_faves%3A20&src=typed_query&f=top"><b>discodiffusion ADVQ ■★</b></a></li>
+            <li><a href="https://twitter.com/nin_artificial/media"><b>nin_artificial</b></a></li>
+            <li><a href="https://www.reddit.com/r/deepdream/"><b>Red.Deepdream</b></a></li>
+            <li><a href="https://twitter.com/GlennIsZen/media"><b>GlennIsZen</b></a>
+                <a href="https://twitter.com/nvnot_/media"><b>NVnot</b></a>
+                <a href="https://twitter.com/snecc_art/media">snecc</a></li>
+            <li><a href="https://twitter.com/ganbrood/media"><b>ganbrood</b></a>
+                <a href="https://twitter.com/Somnai_dreams/media"><b>Somnaidrms</b></a></li>
+            <li><a href="https://twitter.com/romanguillermo_/media"><b>romanguillermo</b></a></li>
+            <li><a href="https://twitter.com/EErratica/media"><b>EncyclErratica</b></a>
+                <a href="https://twitter.com/NekroXIII/media"><b>NecroXIII</b></a></li>
+            <li><a href="https://twitter.com/JvdeNft/media">JvdeNft</a>
+                <a href="https://twitter.com/inigma_a/media">AInigma</a>
+                <a href="https://twitter.com/thibaudz/media">Thibaudz</a></li>
+            <li><a href="https://twitter.com/noir_random/media">noirndom</a>
+                <a href="https://twitter.com/EmporiumLoris/media">LorisEmporium</a></li>
+            <li><a href="https://twitter.com/magnasoma/media">magnasoma</a></li>
+          </ul>
+        </p>
+    </details>
+
+    <h4>TITLE #1</h4>
+    <ul>
+      <li> <a href="URL">LINK #1</a> </li>
+      <li> <a href="URL">LINK #2</a> </li>
+    </ul>
+
+
+    <h4>TITLE #2</h4>
+    <ul>
+      <li> <a href="URL">LINK #3</a> </li>
+      <li> <a href="URL">LINK #4</a> </li>
+      <li> <a href="URL">LINK #5</a> </li>
+    </ul>
+
+    <h4>TITLE #3</h4>
+    <ul>
+      <li> <a href="URL">LINK #6</a> </li>
+      <li> <a href="URL">LINK #7</a> </li>
+    </ul>
+
+    <h4>TITLE #4</h4>
+    <ul>
+      <li> <a href="URL">LINK #7</a> </li>
+      <li> <a href="URL">LINK #8</a> </li>
+    </ul>
+
+
+  </div>
+
+
+
+  <div class="_exa">
+    <script type="text/javascript">
+        document.write(getImageTag());
+    </script>
+  <br />
+    <h4>benkyo shite imasu</h4>
+      <ul>
+      </ul>
+
+      <h4>SENSEI</h4>
+      <ul>
+        <li><a href="https://www.twitter.com/kayoshodo">Kayo sensei</a> - <a href="https://www.youtube.com/watch?v=VNs1NuBKYkc">youtube</a></li>
+        <li><a href="https://www.youtube.com/watch?v=xIPCbiHs-io">Miku sensei</a></li>
+        <li><a href="https://twitter.com/akokitamura">Ako sensei</a> - <a href="https://www.youtube.com/c/akokitamura">youtube</a></li>
+      </ul>
+
+      <h4>ZASSHI</h4>
+      <ul>
+        <li> <a href="https://www.masayume.it/blog/content/classic-japanese-magazines">classic mags ★</a></li>
+        <li> <a href="https://magazinelib.com/japan/">magazine lib</a> </li>
+
+      </ul>
+  </div>
+
+  <div class="_exa">
+    <h4>TITLE #5</h4>
+    <ul>
+      <li> <a href="URL">LINK #10</a> </li>
+      <li> <a href="URL">LINK #11</a> </li>
+      <li> <a href="URL">LINK #12</a> </li>
+      <li> <a href="URL">LINK #13</a> </li>
+    </ul>
+
+    <h4>TITLE #6</h4>
+    <ul>
+      <li> <a href="URL">LINK #14</a> </li>
+      <li> <a href="URL">LINK #15</a> </li>
+      <li> <a href="URL">LINK #16</a> </li>
+      <li> <a href="URL">LINK #17</a> </li>
+    </ul>
+
+  
+  </div>
+
+
+  <div class="_exa">
+    <h4>TITLE #7</h4>
+    <ul>
+      <li> <a href="URL">LINK #10</a> </li>
+      <li> <a href="URL">LINK #11</a> </li>
+      <li> <a href="URL">LINK #12</a> </li>
+      <li> <a href="URL">LINK #13</a> </li>
+    </ul>
+
+    <h4>TITLE #8</h4>
+    <ul>
+      <li> <a href="URL">LINK #14</a> </li>
+      <li> <a href="URL">LINK #15</a> </li>
+      <li> <a href="URL">LINK #16</a> </li>
+      <li> <a href="URL">LINK #17</a> </li>
+    </ul>
+  </div>
+
+  <div class="_exa">
+  <h4>...</h4>
+    <ul>
+    </ul>
+  </div>
+
+
+  <div class="_exa">
+    <h4>...</h4>
+    <ul>
+    </ul>
+  </div>
+
+
+  <div class="_exa">
+    <h4>...</h4>
+    <ul>
+    </ul>
+  </div>
+
+
+  <div class="_exa">
+    <h4>...</h4>
+    <ul>
+    </ul>
+  </div>
+
+
+</div> <!-- container -->
+
+<!-- FOOTER -->
+</body>
+</html>
