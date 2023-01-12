a5fa73d52e849ba53ecb8a1d57cea495f391ae5f
masayume <masayume.blog@gmail.com>
2023-01-12 13:00:56 +0100

    fixes

--- magic-lowres.sh
+++ magic-lowres.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
-ls -lrS ~/DATA/C/pics/cards/ | head -2500 | cut -c 53- | sed 's/.full.jpg$//' | sed 's/\s/%20/g' | awk '{print "<br><a href=https://magiccards.info/query?q=" $1 "&v=cards&s=cname target=_blank>" $1 "</a>"}' > /home/masayume/DATA/C/pics/cards-new-faster/0-magic2download.htm
+# check hostname nmcorradi204859
+
+HOSTNAME=`hostname`
+
+if [ "$HOSTNAME" = "nmcorradi204859" ]; then
+    printf '%s\n' "on the right host"
+else
+    printf '%s\n' "Sorry, script must be run on nmcorradi204859"
+    exit 1
+fi
+
+ls -lrS ~/DATA/C/pics/cards/ | head -2500 | cut -c 53- | sed 's/.full.jpg$//' | sed 's/\s/%20/g' | awk '{print "<br><div style=\"height: 10px; \"><a href=https://magiccards.info/query?q=" $1 "&v=cards&s=cname target=_blank >" $1 "</a></div>"}' > /home/masayume/DATA/C/pics/cards-new-faster/0-magic2download.htm
 
 dolphin /home/masayume/DATA/C/pics/cards-new-faster/
 
