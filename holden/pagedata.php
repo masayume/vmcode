a8f7d223274ad2834744c738af9ce5364cd299e5
masayume <masayume.blog@gmail.com>
2023-05-02 15:38:26 +0200

    cleared debug msg

--- holden/pagedata.php
+++ holden/pagedata.php
@@ -191,7 +191,7 @@ function pagenewtemp($dir, $tag) {
       $thtmlrow .= "<div style=\"width: ${width}px; position: absolute; top: ${toppx}px; right: -8px; text-align: center; padding-right: 2px; padding-left: 2px; padding-top: 4px; padding-bottom: 4px; background-color: #ccc; vertical-align: middle; border-radius: 5px; border:1px solid black;\">";
       $thtmlrow .= '<a href=' . $txtfile . ' target=_blank><b>WF-FILE</b></a>';   // add FILE txt link
       $thtmlrow .= "</div>";
-      print "txtfilepath: $txtfilepath";
+      // print "txtfilepath: $txtfilepath";
     }
 
 
