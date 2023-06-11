3b896713d3dbfc3786d696b689bfd9441593a233
masayume <masayume.blog@gmail.com>
2023-06-11 13:42:28 +0200

    writing

--- home/linux.php
+++ home/linux.php
@@ -128,7 +128,8 @@
 
         <h4>WEB TOOLS 2</h4> <!-- ALSO IN index.php -->
         <ul>
-          <li><a href="/html5/focustxt/"><b>FOCUSTXT TASKS ★</b></a> </li>
+          <li><a href="/html5/focustxt/"><b>FOCUSTXT★</b></a> 
+              <a href="/writing/"><b>WRITING★</b></a> </li>
           <li><a href="https://onlinehtmleditor.dev/"><b>RICH TEXT EDITOR</b></a> </li>
           <li><a href="https://edit.photo/?ref=masayume.it"><b>EDIT PHOTO</b></a>
               <a href="/HTML5/SVGbackgrounds">SVG backs</a></li>
