e91b79c8cb564b4fe5f2cd13ae53563d76acd616
masayume <masayume.blog@gmail.com>
2025-09-11 09:59:31 +0200

    GAMES page

--- home/index.php
+++ home/index.php
@@ -57,7 +57,7 @@
         <li> <a href="retro.php" target="_blank"><b>RETRO</b></a>
              <a href="linux.php" target="_blank"><b>LINUX</b></a> 
              <a href="cosmo/" target="_blank"><b>COSMO</b></a> </li>
-        <li> <a href="unity.php" target="_blank">UNITY</a> 
+        <li> <a href="games/" target="_blank"><b>GAMES</b></a> 
              <a href='/training'>training</a>/<a href="https://www.masayume.it/training">remote</a></li>
         </ul>
     <h4>Solomon's World</h4>
